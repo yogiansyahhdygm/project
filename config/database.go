996d2c528ddcbacf2858ecdbb9a01c26edd780d0
yogiansyahhdygm <yogiansyah.hdygm@gmail.com>
2025-11-09 21:23:11 +0700

    upload

--- config/database.go
+++ config/database.go
@@ -10,22 +10,21 @@ import (
 )
 
 func ConnectDB() (*gorm.DB, error) {
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	pass := os.Getenv("DB_PASSWORD")
-	name := os.Getenv("DB_NAME")
-	ssl := os.Getenv("DB_SSL_MODE")
-	if ssl == "" {
-		ssl = "disable"
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		return nil, fmt.Errorf("DATABASE_URL not set")
 	}
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=Asia/Jakarta", host, user, pass, name, port, ssl)
+	// Railway biasanya butuh SSLMode=require agar koneksi aman
+	// tapi kamu bisa ubah ke disable kalau error SSL
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to connect database: %v", err)
 	}
+
+	fmt.Println("Connected to Railway PostgreSQL!")
 	return db, nil
+
 }
 
 // Auto migrate all models
